150db468883163cf4afbc00fd578d741b84784a1
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-09-18 16:58:30 +0300

    add 2 function for 2 buttons in js/fix source for close icon/fix animation burger menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // --- Burger Close/Open ---
 
-function myFunction() {
+function openBurger() {
+  document.querySelector('.wrap').classList.add('wrap-mobile');
+}
+
+function closeBurger() {
   document.getElementById('burger').classList.toggle('wrap-mobile');
 }
 
