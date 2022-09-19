669419f521294a52e8b10d380baccf82b792cc39
IIIHIMERAIII <87825508+IIIHIMERAIII@users.noreply.github.com>
2022-09-19 07:06:38 +0000

    Deploying to gh-pages from @ IIIHIMERAIII/feedme@ca3968214106e75579fbc0af7f00162b04a992bf 🚀

--- index.82c2af23.js
+++
@@ -1,2 +0,0 @@
-function openBurger(){document.querySelector(".wrap").classList.add("wrap-mobile")}function closeBurger(){document.getElementById("burger").classList.toggle("wrap-mobile")}
-//# sourceMappingURL=index.82c2af23.js.map
